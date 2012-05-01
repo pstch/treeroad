0ae3d2e6e8e0108fa38541e1dc092b1e30fe3a8b
Hugo Geoffroy <hugo@pstch.net>
2012-05-01 23:16:26 +0300

    Added graphTaskView to urls.py

--- treeroad/urls.py
+++ treeroad/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^parseTree/$', 'treeroad.views.parseTree'), # View for parsing the /var/lib/collectd/rrd/* path tree. Should be included into the Admin website later, however it has been implemented earlier for debug purposes.
     url(r'^parseTree/noTest$', 'treeroad.views.parseTree', { 'test' : 0 }), # View for parsing the /var/lib/collectd/rrd/* path tree. Should be included into the Admin website later, however it has been implemented earlier for debug purposes.
     url(r'^servInfo/$', 'treeroad.views.servInfo'),
+    url(r'^graphTask/$', 'treeroad.views.graphTaskView')
 )
