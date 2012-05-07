fcce1f477025805c4edcb9cc671d27630d053a83
Hugo Geoffroy <hugo@pstch.net>
2012-05-07 23:08:11 +0300

    README and added adminplus to settings.py

--- settings.py
+++ settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'area13.treeroad',
     'django_extensions',
+    'adminplus',
 )
 
 # A sample logging configuration. The only tangible logging
