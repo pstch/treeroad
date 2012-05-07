d8cc64698331fe24f6272c36f0b7ea6dc998514c
Hugo Geoffroy <hugo@pstch.net>
2012-05-07 23:38:05 +0300

    Removed admin/parseTree in urls.py : AdminPlus now takes care of that

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
+from adminplus import AdminSitePlus
+
+admin.site = AdminSitePlus()
 admin.autodiscover()
 
 urlpatterns = patterns('',
