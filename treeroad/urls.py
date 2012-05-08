65ebd2d176e9f7c7d00f6094db7ecc9d26231e04
Hugo Geoffroy <hugo@pstch.net>
2012-05-08 22:37:50 +0300

    Removed graphTask and added overView to urls.py

--- treeroad/urls.py
+++ treeroad/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^servInfo/$', 'treeroad.views.servInfo'),
+    url(r'^overView$', 'treeroad.views.overView')
 )
