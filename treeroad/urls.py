a9df8fa7a8c9d87c029b68065c21a047c52e060a
Hugo Geoffroy <hugo@pstch.net>
2012-05-23 13:56:41 +0300

    Added #IGNORE:E1120, seems like PyLint doesn't like *View.as_view()

--- treeroad/urls.py
+++ treeroad/urls.py
@@ -5,13 +5,10 @@ from treeroad.models import node, service, graph
 urlpatterns = patterns('',
     url(r'^$', 'treeroad.views.overView'),
     url(r'^servInfo$', 'treeroad.views.servInfo'),
-    
-    url(r'^nodes$', ListView.as_view(model=node), name='nodeList'),
-    url(r'^node/(?P<pk>\d+)$', DetailView.as_view(model=node), name='nodeDetail'),
-    
-    url(r'^services$', ListView.as_view(model=service), name='serviceList'),
-    url(r'^service/(?P<pk>\d+)$', DetailView.as_view(model=service), name='serviceDetail'),
-    
-    url(r'^graphs$', ListView.as_view(model=graph), name='graphList'),
-    url(r'^graph(?P<pk>\d+)$', DetailView.as_view(model=graph), name='graphDetail'),
+    url(r'^nodes$', ListView.as_view(model=node), name='nodeList'),#IGNORE:E1120
+    url(r'^node/(?P<pk>\d+)$', DetailView.as_view(model=node), name='nodeDetail'),#IGNORE:E1120
+    url(r'^services$', ListView.as_view(model=service), name='serviceList'),#IGNORE:E1120
+    url(r'^service/(?P<pk>\d+)$', DetailView.as_view(model=service), name='serviceDetail'),#IGNORE:E1120
+    url(r'^graphs$', ListView.as_view(model=graph), name='graphList'),#IGNORE:E1120
+    url(r'^graph/(?P<pk>\d+)$', DetailView.as_view(model=graph), name='graphDetail'),#IGNORE:E1120
 )
