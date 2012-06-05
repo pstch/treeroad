597a7f072dc9401f7932c362313fad414792a0df
Hugo Geoffroy <hugo@pstch.net>
2012-06-05 16:53:41 +0300

    Readded root redirect (from / to /treeroad). NOTE: Test DB will be flushed because of recent changes in models.py

--- urls.py
+++ urls.py
@@ -2,15 +2,15 @@ from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
 from django.http import HttpResponseRedirect
-from treeroad.adminsite import TreeroadAdminSite
+from treeroad.adminsite import AdminSitePlus
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site = TreeroadAdminSite()
+admin.site = AdminSitePlus()
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$',  lambda x: HttpResponseRedirect('/treeroad/')),
+    url(r'^$',  lambda x: HttpResponseRedirect('/treeroad/')),
     url(r'^treeroad/', include('area13.treeroad.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
